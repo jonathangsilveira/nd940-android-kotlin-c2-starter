c5c03520ab91bbddf8b97ce12140442b44605e60
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2021-01-09 09:26:21 -0300

    Dynamic ImageView's contentDescription

--- starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import com.udacity.asteroidradar.data.remote.PictureOfDay
 
@@ -18,10 +19,13 @@ fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
 
 @BindingAdapter("asteroidStatusImage")
 fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
+    val resources = imageView.context.resources
     if (isHazardous) {
         imageView.setImageResource(R.drawable.asteroid_hazardous)
+        imageView.contentDescription = resources.getText(R.string.potentially_hazardous_asteroid_image)
     } else {
         imageView.setImageResource(R.drawable.asteroid_safe)
+        imageView.contentDescription = resources.getText(R.string.not_hazardous_asteroid_image)
     }
 }
 
@@ -54,11 +58,7 @@ fun isLoading(view: View, isLoading: Boolean) {
 @BindingAdapter("pictureOfDay")
 fun bindPictureOfDay(imageView: ImageView, pictureOfDay: PictureOfDay?) {
     if (pictureOfDay?.mediaType == "image")
-        Picasso.get()
-            .load(pictureOfDay.url)
-            .placeholder(R.drawable.loading_animation)
-            .error(R.drawable.ic_broken_image)
-            .into(imageView)
+        imageView.loadFrom(pictureOfDay)
     else
         imageView.setImageResource(R.drawable.placeholder_picture_of_day)
 }
@@ -66,5 +66,31 @@ fun bindPictureOfDay(imageView: ImageView, pictureOfDay: PictureOfDay?) {
 @BindingAdapter("imageTitle")
 fun bindPictureOfDayTitle(textView: TextView, pictureOfDay: PictureOfDay?) {
     val resources = textView.context.resources
-    textView.text = pictureOfDay?.title ?: resources.getText(R.string.image_of_the_day)
+    val title = pictureOfDay?.title ?: resources.getString(R.string.image_of_the_day)
+    textView.text = title
 }
+
+private fun ImageView.loadFrom(pictureOfDay: PictureOfDay?) {
+    val imageView = this
+    imageView.contentDescription = imageView.loadingContentDescription
+    val callback = object : Callback {
+        override fun onSuccess() {
+            imageView.contentDescription = imageView.loadedContentDescription(pictureOfDay?.title)
+        }
+        override fun onError(e: Exception?) {}
+    }
+    Picasso.get()
+        .load(pictureOfDay?.url)
+        .placeholder(R.drawable.loading_animation)
+        .error(R.drawable.ic_broken_image)
+        .into(this, callback)
+}
+
+private val ImageView.loadingContentDescription: CharSequence
+    get() = this.context.resources.getText(R.string.this_is_nasa_s_picture_of_day_showing_nothing_yet)
+
+private fun ImageView.loadedContentDescription(title: String?) =
+    this.context.resources.getText(
+        R.string.nasa_picture_of_day_content_description_format,
+        title
+    )
