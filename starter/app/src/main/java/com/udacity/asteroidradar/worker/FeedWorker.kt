c4016bc4a131b946ee404720150b4c0681f2b987
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2021-01-14 14:00:10 -0300

    Remove loggers

--- starter/app/src/main/java/com/udacity/asteroidradar/worker/FeedWorker.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/worker/FeedWorker.kt
@@ -1,7 +1,6 @@
 package com.udacity.asteroidradar.worker
 
 import android.content.Context
-import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.udacity.asteroidradar.di.ServiceLocator
@@ -15,15 +14,11 @@ class FeedWorker(
     override suspend fun doWork(): Result {
         val repo = ServiceLocator.provideAsteroidsRepository(applicationContext)
         return try {
-            Log.d(WORK_NAME, "Fetching...")
             repo.fetchAndSaveAsteroids()
-            Log.d(WORK_NAME, "Done!")
             Result.success()
         } catch (e: HttpException) {
-            Log.d(WORK_NAME, "Retry!")
             Result.retry()
         } catch (e: Exception) {
-            Log.d(WORK_NAME, "Failure!")
             Result.failure()
         }
     }
