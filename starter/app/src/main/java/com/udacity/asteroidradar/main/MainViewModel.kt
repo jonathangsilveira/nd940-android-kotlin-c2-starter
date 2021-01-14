4a65781386d995099bbe6059743487138673cfe0
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2021-01-14 09:56:51 -0300

    Re order code

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -12,12 +12,12 @@ class MainViewModel(private val repo: AsteroidsRepository) : ViewModel() {
 
     private val filters = MutableLiveData(AsteroidsRepository.Filters.DAY)
 
+    private val workInfoLiveData: LiveData<MutableList<WorkInfo>> = WorkManager.getInstance()
+        .getWorkInfosByTagLiveData(FeedWorker.WORK_NAME)
+
     val pictureOfDay: LiveData<PictureOfDay?> = repo.pictureOfDay
 
     val asteroids: LiveData<List<Asteroid>> = filters.switchMap(repo::getAsteroids)
-
-    private val workInfoLiveData: LiveData<MutableList<WorkInfo>> = WorkManager.getInstance()
-        .getWorkInfosByTagLiveData(FeedWorker.WORK_NAME)
 
     val loading = workInfoLiveData.map { workInfoList ->
         val workInfo = workInfoList.firstOrNull()
